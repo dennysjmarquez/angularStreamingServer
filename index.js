5531b46c1b2741fd30e939f3d62be7b4989cf222
Dennys J Marquez <dennysjmarquez@gmail.com>
2022-05-03 19:08:50 -0400

    Configuración para servir cualquier página estática, idea para agregar la versión compilada para producción de angularStreaming, lo compilas y lo agregas en la carpeta public de este servidor remplazando el index.html por el de tu proyecto

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require('dotenv-flow').config();
 const { CORS_CLIENT_ORIGINS, SERVER_PORT } = require('./constant');
 const express = require('express');
 const cors = require('cors');
+const path = require("path");
 
 // crea el servidor de express
 const app = new express();
@@ -22,6 +23,14 @@ app.use(function (err, req, res, next) {
 	} else {
 		next(err);
 	}
+});
+
+// Directorio público, Pagina estática
+app.use(express.static('public'));
+
+// Si no está en las rutas se pasa por la index.html
+app.use('*', (req, res) => {
+	res.sendFile(path.resolve(__dirname, 'public/index.html'));
 });
 
 // Inicia el servidor de express
